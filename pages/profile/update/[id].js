77f5681e6416b1c72cd0402d2fb3dad673ebbedd
sohanr-cyber <sohanur25800@gmail.com>
2023-12-24 06:53:44 +0600

    sdjglj

--- pages/profile/update/[id].js
+++ pages/profile/update/[id].js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
-import Personal from '@/components/Profile/Personal'
+import Personal from '@/components/Profile/Update/Personal'
 import Education from '@/components/Profile/Update/Education'
-import Physical from '@/components/Profile/Physical'
+import Physical from '@/components/Profile/Update/Physical'
 import Religion from '@/components/Profile/Update/Religion'
-import Address from '@/components/Profile/Address'
+import Address from '@/components/Profile/Update/Address'
 import Basic from '@/components/Profile/Update/Basic'
-import Family from '@/components/Profile/Family'
-import Expectation from '@/components/Profile/Expectation'
+import Family from '@/components/Profile/Update/Family'
+import Expectation from '@/components/Profile/Update/Expectation'
 
 const Update = () => {
   return (
